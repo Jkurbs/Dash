5bed0fa27362956102929524aca18c9626531652
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2021-06-16 14:50:22 -0400

    second commit

---
+++ DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashApp.swift
+//  Dash
+//
+//  Created by Kerby Jean on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
