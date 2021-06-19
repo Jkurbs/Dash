671f386f726cb5b4969f788d135c9fa295336586
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2021-06-18 21:20:25 -0400

    Fix minor issue

--- Dash/ContentView.swift
+++ Dash/ContentView.swift
@@ -7,42 +7,43 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-        
-    var viewModel = ViewModel()
+    
+    private var viewModel = ViewModel()
     
     var body: some View {
-            NavigationView {
-                List {
-                    if viewModel.list() != nil {
-                        ForEach(viewModel.list()!, id: \.self) { item in
-                            NavigationLink( destination: DestinationView(items: Array(item[1..<item.count]))) {
-                                Text(item.first!).font(.subheadline)
-                            }
+        NavigationView {
+            List {
+                if viewModel.list() != nil {
+                    ForEach(viewModel.list()!, id: \.self) { item in
+                        NavigationLink( destination: DestinationView(items: Array(item[1..<item.count]))) {
+                            Text(item.first!).font(.subheadline)
                         }
                     }
                 }
-                .navigationBarTitle("Dash")
             }
+            .navigationBarTitle("Dash")
         }
     }
+}
 
-    struct DestinationView : View {
-        var items: [Substring]
-        var body: some View {
+struct DestinationView : View {
+    var items: [Substring]
+    var body: some View {
+        if self.items.count > 0 {
             List {
-                if self.items.count > 0 {
-                    NavigationLink( destination: DestinationView(items: Array(items[1..<items.count]))) {
-                        Text(items[0]).font(.subheadline)
-                    }
-                } 
+                NavigationLink( destination: DestinationView(items: Array(items[1..<items.count]))) {
+                    Text(items[0]).font(.subheadline)
+                }
             }
-        }
+        } 
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
